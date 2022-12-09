b5f475ab5fed3edbc436fb23822cb1ed1ee13bdf
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-10 06:11:48 +0700

    Updated login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import styles from "../styles/Login"
+
 const Login = () => {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
+  return <View style={styles.container}>
+
+  </View>;
 }
 
 export default Login
