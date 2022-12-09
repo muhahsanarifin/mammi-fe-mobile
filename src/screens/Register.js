1dccd71d6eda424a793eba8cc8203edbb712880b
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-10 06:12:13 +0700

    Updated register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Image, TextInput} from 'react-native';
+import Form from 'react-native-form';
+import React from 'react';
+
+import styles from '../styles/Register';
+
+import Button from '../components/Button';
 
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.container}>
+      <View style={styles.imageSection}>
+        <Image
+          source={require('../assets/images/girl-chilling-exploring-on-the-phone.png')}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.formSection}>
+        <Form style={styles.form}>
+          <TextInput
+            placeholder="Enter your email adress"
+            style={styles.textInput}
+          />
+          <TextInput
+            placeholder="Enter your password"
+            style={styles.textInput}
+          />
+          <TextInput
+            placeholder="Enter your phone number"
+            style={styles.textInput}
+          />
+          <Button
+            onPress={() => navigation.push('Register')}
+            styleBtn={styles.btnRegister}
+            styleText={styles.btnRegisterText}
+            titleText={`Create New Account`}
+          />
+        </Form>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
---
+++ src/styles/Register.js
@@ -0,0 +1,74 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: 'green',
+    backgroundColor: '#F2F2F2',
+    flex: 1,
+    flexDirection: 'column',
+    padding: 20,
+  },
+  imageSection: {
+    // backgroundColor: 'yellow',
+    position: 'relative',
+    flex: 1,
+    flexDirection: 'row',
+    position: 'relative',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 400,
+    height: 400,
+    position: 'absolute',
+    alignSelf: 'center',
+  },
+  formSection: {
+    // backgroundColor: 'red',
+    position: 'relative',
+    flex: 1,
+    flexDirection: 'row',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  form: {
+    // backgroundColor: 'blue',
+    display: 'flex',
+    flexDirection: 'column',
+    width: '85%',
+  },
+
+  textInput: {
+    position: 'relative',
+    fontFamily: 'Poppins',
+    marginTop: 10,
+    marginBottom: 10,
+    width: 'auto',
+    borderBottomWidth: 1,
+    borderBottomColor: '#9A9A9D',
+    fontSize: 17,
+    color: '#9A9A9D',
+    fontWeight: '600'
+  },
+  btnRegister: {
+    backgroundColor: '#6A4029',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    height: 65,
+    borderRadius: 20,
+    width: 300,
+    alignSelf: 'center',
+  },
+  btnRegisterText: {
+    fontSize: 17,
+    fontWeight: '800',
+    fontFamily: 'Poppins',
+    color: '#FFFFFF',
+  },
+});
+
+export default styles;
