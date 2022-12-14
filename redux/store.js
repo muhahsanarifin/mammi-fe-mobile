8b8c9eeceef9138fee31e3f670619439cfc72655
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-14 07:14:01 +0700

    Updated store

--- redux/store.js
+++ redux/store.js
@@ -1,20 +1,22 @@
 import {configureStore} from '@reduxjs/toolkit';
-// import regSlice from "./reducers/regSlice"
 import {persistStore, persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 import reducers from './reducers';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import logger from 'redux-logger';
 
 const persistConfig = {
   key: 'Mammi',
-  storage,
+  storage: AsyncStorage,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({thunk: true, serializableCheck: false}),
+    getDefaultMiddleware({thunk: true, serializableCheck: false}).concat(
+      logger,
+    ),
 });
 
 export const persistedStore = persistStore(store);
