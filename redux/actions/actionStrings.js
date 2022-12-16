5c871f67a79b3a1a40b907a823ecae55f5eebc12
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-16 08:38:06 +0700

    Add actionString.js

---
+++ redux/actions/actionStrings.js
@@ -0,0 +1,33 @@
+export const actionStrings = {
+  // TODO: auth action string
+  authLogin: 'AUTH_LOGIN',
+  authLogout: 'AUTH_LOGOUT',
+  getUsers: 'USERS_GET',
+
+  // TODO: user action string
+  deleteUser: 'USER_DELETE',
+  userRegister: 'USER_REGISTER',
+  editPassword: 'PASSWORD_EDIT',
+
+  // TODO: profile action string
+  editProfile: 'PROFILE_EDIT',
+  getProfileContact: 'PROFILE_CONTACT_GET',
+  getProfileDetail: 'PROFILE_DETAIL_GET',
+
+  // TODO: product action string
+  getProducts: 'PRODUCTS_GET',
+  editProduct: 'PRODUCT_EDIT',
+  deleteProduct: 'PRODUCT_DELETE',
+  createProduct: 'PRODUCT_CREATE',
+
+  // TODO: transactions action string
+  getTransactions: 'TRANSACTIONS_GET',
+  createTransaction: 'TRANSACTIONS_CREATE',
+  editTransaction: 'TRANSACTION_EDIT',
+  deleteTransaction: 'TRANSACTION_DELETE',
+  getHistoryransaction: 'TRANSACTION_HISOTY_GET',
+
+  // TODO: promos action string
+  getPromos: 'PROMOS_GET',
+  createPromo: 'PROMO_CREATE',
+};
