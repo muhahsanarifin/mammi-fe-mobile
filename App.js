2784f5c49b714023aa11635da1a3671dec22a365
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-18 10:42:03 +0700

    Updated App.js

--- App.js
+++ App.js
@@ -1,114 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-// TODO: import routes
-// import Stacks from "./routes/stacks"
-
-// TODO: import screens
-import Welcome from './src/screens/Welcome';
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
-import ForgotPassword from './src/screens/ForgotPassword';
-import Home from './src/screens/Home';
-import Orders from './src/screens/Orders';
-import ProductDetail from './src/screens/ProductDetail';
-import History from './src/screens/History';
-import Checkout from './src/screens/Checkout';
-import Cart from './src/screens/Cart';
-import Profile from './src/screens/Profile';
-import EditProfile from './src/screens/EditProfile';
-import Chat from './src/screens/Chat';
-import Payment from './src/screens/Payment';
-// import Favorite from "./src/screens/Favorite";
-import Products from './src/screens/Products';
+import Stacks from './routes/stacks';
 
 const App = () => {
-  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="ForgotPassword"
-          component={ForgotPassword}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Products"
-          component={Products}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="ProductDetail"
-          component={ProductDetail}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Orders"
-          component={Orders}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Cart"
-          component={Cart}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Checkout"
-          component={Checkout}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Payment"
-          component={Payment}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="History"
-          component={History}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="EditProfile"
-          component={EditProfile}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-          options={{headerShown: false}}
-        />
-        {/* <Stack.Screen
-          name="Favorite"
-          component={Favorite}
-          options={{headerShown: false}}
-        /> */}
-      </Stack.Navigator>
+      <Stacks />
     </NavigationContainer>
   );
 };
