feda95664e4c3e90b07090ce115095c9093e9eed
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-19 09:37:19 +0700

    Updated index.js

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-// import {Provider} from 'react-redux';
-// import {PersistGate} from 'redux-persist/integration/react';
-// import store, {persistedStore} from './redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import store, {persistedStore} from './redux/store';
+import React from 'react';
 
-// const Redux = () => (
-//   <Provider store={store}>
-//     <PersistGate persistor={persistedStore} loading={null}>
-//       <App/>
-//     </PersistGate>
-//   </Provider>
-// );
+const AppWR = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistedStore} loading={null}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppWR);
