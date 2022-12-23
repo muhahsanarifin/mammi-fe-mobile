a35fefaa2adaa369c02577473e502d5c2359582d
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-23 08:45:59 +0700

    Updated Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,91 +1,121 @@
-import {View, Text, Image, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  ToastAndroid,
+  KeyboardAvoidingView,
+  ScrollView
+} from 'react-native';
 import Form from 'react-native-form';
-import React from 'react';
-import {useState} from 'react';
+import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import Axios from 'axios';
-
 import styles from '../styles/Login';
 import Button from '../components/Button';
+import PasswordToggle from '../components/PasswordToggle';
+import {useDispatch} from 'react-redux';
+import authAction from '../../redux/actions/auth';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = () => {
   const navigation = useNavigation();
-
-  // TODO: Test API
+  const [show, setShow] = useState(true);
+  const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const tryToHandleLogin = async () => {
-    const data = {
-      email,
-      password,
-    };
-    try {
-      const response = await Axios.post(`http://192.168.18.90:8080/api/v1/auth`,data
-      );
-      console.log(response);
-      navigation.push('Home');
-    } catch (error) {
-      console.log(error);
-    }
+
+  const body = {
+    email,
+    password,
   };
 
-  // TODO: Pass to Home screen to build styling.
-  // const tryToHandleLogin = () => {
-  //   navigation.push('Home')
-  // }
+  const loginSuccess = success => {
+    ToastAndroid.showWithGravityAndOffset(
+      `${success}`,
+      ToastAndroid.LONG,
+      ToastAndroid.TOP,
+      0,
+      25,
+    );
+
+    navigation.replace('Home');
+  };
+
+  const loginFailed = error => {
+    ToastAndroid.showWithGravityAndOffset(
+      `${error}`,
+      ToastAndroid.LONG,
+      ToastAndroid.TOP,
+      0,
+      25,
+    );
+  };
+
+  const handleLogin = () => {
+    dispatch(authAction.loginThunk(body, loginSuccess, loginFailed));
+    setEmail('');
+    setPassword('');
+  };
+
+  // TODO: Handle show/hide password
+  const handlePasswodToggle = () => {
+    setShow(!show);
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageSection}>
-        <Text style={styles.titleText}>Log in</Text>
-        <Image
-          source={require('../assets/images/lady-having-coffee.png')}
-          style={styles.image}
-        />
-      </View>
-      <View style={styles.formSection}>
-        <Form style={styles.form}>
-          <TextInput
-            name="email"
-            value={email}
-            placeholder="Enter your email adress"
-            style={styles.textInput}
-            onChangeText={value => setEmail(value)}
+    <KeyboardAvoidingView style={styles.container}>
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.imageSection}>
+          <Image
+            source={require('../assets/images/lady-having-coffee.png')}
+            style={styles.image}
           />
-          <TextInput
-            name="password"
-            value={password}
-            placeholder="Enter your password"
-            style={styles.textInput}
-            secureTextEntry={true}
-            onChangeText={value => setPassword(value)}
-          />
+          <Text style={styles.titleText}>Log in</Text>
+        </View>
+        <View style={styles.formSection}>
+          <Form style={styles.form}>
+            <TextInput
+              value={email}
+              placeholder="Enter your email address"
+              style={styles.textInputFirst}
+              onChangeText={value => setEmail(value)}
+            />
+            <View style={styles.textInputxPasswordToggle}>
+              <TextInput
+                value={password}
+                placeholder="Enter your password"
+                style={styles.textInputSecond}
+                secureTextEntry={show}
+                onChangeText={value => setPassword(value)}
+              />
+              <PasswordToggle onPress={handlePasswodToggle} show={show} />
+            </View>
 
-          <Text
-            style={styles.textForgotPassword}
-            onPress={() => navigation.push('ForgotPassword')}>
-            Forgot Password?
-          </Text>
+            <Text
+              style={styles.textForgotPassword}
+              onPress={() => navigation.push('ForgotPassword')}>
+              Forgot Password?
+            </Text>
 
-          <Button
-            // onPress={handleLogin}
-            onPress={tryToHandleLogin}
-            styleBtn={styles.btnLogin}
-            styleText={styles.btnLoginText}
-            titleText={`Login`}
-          />
-          <Text style={styles.titleTextFormSection}>or login in with</Text>
-          <Button
-            // onPress={handleLogin}
-            sourceImg={require('../assets/icons/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png')}
-            styleImg={styles.iconGoogle}
-            styleBtn={styles.btnGoogle}
-            styleText={styles.btnGooleText}
-            titleText={`Login With Google`}
-          />
-        </Form>
-      </View>
-    </View>
+            <Button
+              onPress={handleLogin}
+              styleBtn={styles.btnLogin}
+              styleText={styles.btnLoginText}
+              titleText={`Login`}
+            />
+            <Text style={styles.titleTextFormSection}>or login in with</Text>
+            <Button
+              // onPress={handleLoginViaGoogle}
+              sourceImg={require('../assets/icons/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png')}
+              styleImg={styles.iconGoogle}
+              styleBtn={styles.btnGoogle}
+              styleText={styles.btnGooleText}
+              titleText={`Login With Google`}
+            />
+          </Form>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
